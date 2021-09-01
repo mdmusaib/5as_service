2cf1b88fc57704616b56082e329770d08cd63bc7
Mohammed Musaib <musaibkm@gmail.com>
2021-09-01 11:04:35 +0530

    committing change12345

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -161,7 +161,7 @@ class ProjectController extends Controller
             foreach ($getSelectedDropdown as $selectedService) {
                 $obj->service=$selectedService->service_id;
                 $obj->employee=$selectedService->employee_id;
-                array_push($arr,$obj);
+                array_push($arr,{"service":$selectedService->service_id,"employee":$selectedService->employee_id});
 
             }
             $responseObj->service_dropdown=$arr;
